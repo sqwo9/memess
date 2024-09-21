84227517243a6f55b6fb7fdaf5f2e50aad6a558c
Zuev I <zuevivan423@gmail.com>
2024-09-21 06:53:54 +0300

    Image shower

--- show_meme.py
+++ show_meme.py
@@ -4,12 +4,10 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    image_path = 'memes/mem.jpg'
+    image_path = 'memes1/i1.jpg'
 
     img = Image.open(image_path)
 
     plt.imshow(img)
     plt.axis('off')
     plt.show()
-
-
